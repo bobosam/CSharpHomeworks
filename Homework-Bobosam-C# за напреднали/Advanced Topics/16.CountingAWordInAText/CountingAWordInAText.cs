7327e16b81b5269a8871a4ed77d842a50843f7ba
Boyko <Andonov>
2014-12-23 06:46:54 +0200

    commit homework

---
+++ Homework-Bobosam-C# за напреднали/Advanced Topics/16.CountingAWordInAText/CountingAWordInAText.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+
+class CountingAWordInAText
+{
+    static void Main()
+    {
+        Console.SetIn(new StreamReader(Console.OpenStandardInput(8192)));
+        Console.Write("Word --> ");
+        string word = Console.ReadLine();
+        Console.Write("Text --> ");
+        string text = Console.ReadLine();
+        int counter = 0;
+        bool check = true;
+
+        string[] words = text.Split(' ', ',', '.','"','?','!','/','@',':','(',')');
+
+        for (int i = 0; i < words.Length; i++)
+        {
+            if (words[i].Length == word.Length)
+            {
+                for (int j = 0; j < word.Length; j++)
+                {
+                    string tmpWord = word.Substring(j,1);
+                    string tmpText = words[i].Substring(j,1);
+                    if ( tmpText.ToUpper() != tmpWord.ToUpper())
+                    {
+                        check = false;
+                    }
+                }
+
+                if (true == check)
+                {
+                    counter++;
+                }
+
+                check = true;
+            }
+        }
+
+        Console.WriteLine("{0} -- > {1}", word, counter);
+
+
+    }
+}
---
+++ Homework-Bobosam-C# за напреднали/Advanced Topics/Problem 5. SortingNumbers/SortingNumbers.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+class SortingNumbers
+{
+    static void Main()
+    {
+        bool check;
+        int numberN = 0;
+
+        do
+        {
+            Console.Write("N --> ");
+            check = int.TryParse(Console.ReadLine(), out numberN);
+        } while (false == check);
+
+        int[] numbers = new int[numberN];
+
+        for (int i = 0; i < numberN; i++)
+        {
+            do
+            {
+                Console.Write("{0} --> ", i + 1);
+                check = int.TryParse(Console.ReadLine(), out numbers[i]);
+            } while (false == check);
+        }
+        Console.WriteLine();
+        Array.Sort(numbers);
+
+        foreach (var number in numbers)
+        {
+            Console.WriteLine(number);
+        }
+        Console.WriteLine();
+    }
+}
